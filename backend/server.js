e2dc1020fbe75021e1c8c3804c4c414c0b9cb4bc
ikhan1-git <imtiyaz.khan@grazitti.com>
2025-06-14 19:14:16 +0530

    delete vercel files

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/tourSettings', tourSettings);
 
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'api')));
 
 // Error handling middleware
 // app.use((err, req, res, next) => {
